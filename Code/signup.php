abea8ec40f4723c127712e46b821ee4f437326d0
aneridesai23 <45923314+aneridesai23@users.noreply.github.com>
2020-03-16 19:01:48 -0400

    Update signup.php

--- Code/signup.php
+++ Code/signup.php
@@ -1,39 +1,28 @@
-<?php include('server.php');?>
-
+<?php include('server.php')?>
 <html>
 	<head>
-		<title> Signup Form </title>
+		<title> Login Form Design</title>
 		<link rel="stylesheet" type="text/css" href="styles.css">
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-		<script>
-			$(document).ready(function(){
-				$('.check').click(function() {
-					$('.check').not(this).prop('checked', false);
-				});
-			});
-</script>
 	</head>
 <body>
-		<div id="login-box">
-			<div class = "left-box">
-			<h1 align="center"> Sign up </h1>
-			<form action="signup.php" method="post">
+		<div class = "login-box">
+			<h1 align="center"> Login </h1>
+			<form action="login.php" method="post">
 				<?php include('errors.php');?>
-				<input type = "text" name = "username" placeholder="Username" value="<?php echo $username; ?>"/>
-				<input type = "text" name = "email" placeholder="Email" value="<?php echo $email; ?>"/>
-				<input type = "password" name = "password" placeholder="Password"/>
-				<input type = "password" name = "password2" placeholder="Confirm Password"/>
-				<input type="checkbox" name="Driver" class="check" value = "Driver"/>Driver 
-				<input type="checkbox" name="Sponser" class="check" value = "Sponser"/>Sponser 
-				<input type="checkbox" name="Administrator" class="check" value="Administrator"/>Administrator
-				<button type="submit" name = "signup_user">Submit</button>
+				<p>Email</p>
+				<input type = "text" name = "username" placeholder="Enter your username"/>
+				<p>Password</p>
+				<input type = "password" name = "password" placeholder="Enter your password"/>
+				<button type="submit" name = "login_user">Login</button>
 				<div class = "para-text">
-					<p>
-						<a href="login.php">Already have an account?: Sign up</a>
-					</p>
-				</div>
+				<p>
+					<a href="forgotPass.html">Forgot your password?: Click here!!</a>
+				</p>
+				<p>
+				  <a href="signup.php">Don't have an account?: Sign up</a>
+				</p>
+				</div>				
 			</form>
-		</div>
 	</div>
 </body>
 </html>
