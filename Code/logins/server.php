959e708158e8ca2d791313757bb77acc8d10b42e
aneridesai23 <aneri.desai@gmail.com>
2020-03-19 11:49:33 -0400

    Login and signup completed.

--- Code/logins/server.php
+++ Code/logins/server.php
@@ -4,6 +4,7 @@ session_start();
 // initializing variables
 $username = "";
 $email    = "";
+$role     = "";
 $errors = array(); 
 
 // connect to the database
@@ -17,12 +18,12 @@ if (isset($_POST['signup_user'])) {
   $password_1 = mysqli_real_escape_string($db, $_POST['password']);
   $password_2 = mysqli_real_escape_string($db, $_POST['password2']);
 
-  if (empty($_POST['Driver']) && empty($_POST['Sponser'])) {
+  if (empty($_POST['Driver']) && empty($_POST['Sponsor'])) {
 	  $role = mysqli_real_escape_string($db, $_POST['Administrator']);
-  }elseif(empty($_POST['Sponser']) && empty($_POST['Administrator'])) {
+  }elseif(empty($_POST['Sponsor']) && empty($_POST['Administrator'])) {
 	  $role = mysqli_real_escape_string($db, $_POST['Driver']);
   }else{
-	  $role = mysqli_real_escape_string($db, $_POST['Sponser']);
+	  $role = mysqli_real_escape_string($db, $_POST['Sponsor']);
   }
 	
   // form validation: ensure that the form is correctly filled ...
@@ -34,7 +35,7 @@ if (isset($_POST['signup_user'])) {
 	array_push($errors, "The two passwords do not match");
   }
   if(empty($role)) { array_push($errors, "Choose: Driver/Sponsor/Administrator");}
-
+  
   // first check the database to make sure 
   // a user does not already exist with the same username and/or email
   $user_check_query = "SELECT * FROM users WHERE username='$username' OR email='$email' LIMIT 1";
@@ -58,9 +59,7 @@ if (isset($_POST['signup_user'])) {
   	$query = "INSERT INTO users (username, email, password, role) 
   			  VALUES('$username', '$email', '$password', '$role')";
   	mysqli_query($db, $query);
-  	$_SESSION['username'] = $username;
-  	$_SESSION['success'] = "You are now logged in";
-  	header('location: index.php');
+  	header('location: login.php');
   }
 }
 
@@ -80,10 +79,21 @@ if (isset($_POST['login_user'])) {
   	$password = md5($password);
   	$query = "SELECT * FROM users WHERE username='$username' AND password='$password'";
   	$results = mysqli_query($db, $query);
+	$user = mysqli_fetch_assoc($results);
+	  
   	if (mysqli_num_rows($results) == 1) {
-  	  $_SESSION['username'] = $username;
+  	  /*$_SESSION['username'] = $username;
   	  $_SESSION['success'] = "You are now logged in";
-  	  header('location: index.php');
+  	  header('location: index.php');*/
+	  if($user['role'] === "Driver"){
+		  header('location: ../profiles/driver_home.html');
+	  }elseif($user['role'] === "Sponsor"){
+		  header('location: ../profiles/sponsor_home.html');
+	  }elseif($user['role'] === "Administrator"){
+		  header('location: ../profiles/admin_home.html');
+	  }else {
+		  array_push($errors, "There is an error");
+	  }
   	}else {
   		array_push($errors, "Wrong username/password combination");
   	}
