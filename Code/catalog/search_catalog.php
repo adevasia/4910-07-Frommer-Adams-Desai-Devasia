d6fb4ec4fca401cc0cb8b0be169c0584cd2eec63
cnfrommer <48249374+cnfrommer@users.noreply.github.com>
2020-04-09 15:30:34 -0400

    Add files via upload

--- Code/catalog/search_catalog.php
+++ Code/catalog/search_catalog.php
@@ -1,7 +1,5 @@
 <?php include 'finding_catalog.php';?>
-<?php include '../profiles/sponsor_catergories.php';?>
-
-
+<?php include('../server.php');?>
 
 <!DOCTYPE html>
 <html>
@@ -12,14 +10,13 @@
 <link rel="stylesheet" type="text/css" href="catalog_items.css">
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
 </head>
 
 <body class="style">
 <ul>
 	<h2 class="pointer">POINTS: </h2>
 
-	 <div class="dropdown">
+<div class="dropdown">
           <button class="dropbtn"><a href="../profiles/driverprof.html"><img src="../profiles/profpic.png" alt="Avatar" width="50" height="50" >
           </a></button>
           <div class="dropdown-content">
@@ -28,6 +25,7 @@
             <a href="../logins/login.php">LOGOUT</a>
           </div>
         </div>
+
 
 	<button class="dropbtn2"><li><a href="shoppingCart.html"><img src="cart.png" alt="Cart" width="50" height="50"  style="float: right;"></a></li></button>
 
@@ -38,22 +36,21 @@
 <!-- The search form -->
 <form class="example" action="search_catalog.php" method="post" style="margin:auto;max-width:500px">
 	<input type="text" id="user_input" placeholder="Search..." name="search2">
-	<button id="subButton" type="submit" name="search" onclick="myFunc()"><i class="fa fa-search"></i></button>
+	<button id="subButton" type="submit" onclick="myFunc()"><i class="fa fa-search"></i></button>
 </form>
 
 <br style = “line-height:10”>
 
 <!--Categories-->
-<form class="buttons" action="catalog_home.php" method="post">	
-	<button class="active" data-sort="sort-all" type="submit" name="cat" value="<?php echo $_SESSION['category1'];?>" > <?php echo $_SESSION['category1']?></button>
-	<button type="submit" name="cat" value="<?php echo $_SESSION['category2'];?>"><?php echo $_SESSION['category2']?></button>
-	<button type="submit" name="cat" value="<?php echo $_SESSION['category3'];?>"><?php echo $_SESSION['category3']?></button>
-	<button type="submit" name="cat" value="<?php echo $_SESSION['category4'];?>"><?php echo $_SESSION['category4']?></button>
-</form>
+<div class="buttons">
+	<button class="active" data-sort="sort-all">All</button>
+	<button>Recommended</button>
+	<button>Pants</button>
+	<button>Shirts</button>
+</div>
   
 <br style = “line-height:10”>
-<?php echo $query?>
-	
+
 <!-- Load Font Awesome Icon Library -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
@@ -62,14 +59,24 @@
 <button onclick="gridView()"><i class="fa fa-th-large"></i> Grid</button>
 <br style = “line-height:10”>
 
+<form action="search_catalog.php" method="post">
 <table class="column">
   <tr>
   <td >
-    <?php echo $results?>
+    <input type="text" name="t" placeholder="title" value="<?php session_start(); echo $search[2]; ?>" />
+    <?php 
+    echo "<tr><td><img src=\"$search[0]\"></td><td><a href=\"$search[1]\">$search[2]</a><br>$$search[3]</td></tr>"; ?> 
+     
   </td>
 </tr>
 </table>
 
+<br><br>
+
+
+<button type="submit" name = "add">Add to Cart</button>
+<input type="text" name="p" placeholder="price of item" value="<?php echo $search[3]; ?>" />
+</form>
 
 <script src="catalog_items.js"></script>
 </body>
