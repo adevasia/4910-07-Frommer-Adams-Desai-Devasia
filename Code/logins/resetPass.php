f3fddb66ab77c409042848d7f6dad914fae467d4
aneridesai23 <aneri.desai@gmail.com>
2020-04-09 16:01:37 -0400

    password reset

--- Code/logins/resetPass.php
+++ Code/logins/resetPass.php
@@ -5,15 +5,16 @@
 	</head>
 <body>
 		<div class = "pass-box"> <br><br>
-			<h1 align="center"> Forgot Password? </h1>
+			<h1 align="center"> Choose your new password </h1>
 			<form>
 				<p>You can reset your password here.</p>
 				<br>
 				<div class="box">
-				<input type = "text" name = "email" placeholder="email address"/>
+				<input type = "password" name = "pass1" placeholder="Password"/>
+				<input type = "password" name = "pass2" placeholder="Confirm Password"/>
 			</div>
 				<br> 
-				<input type="submit" formaction="login.html" name = "" value="Reset Password"/>
+				<button type="submit" name = "pass_change">Reset Password</button>
 			</form>
 	</div>
 </body>
