e6dd5ba1bd3629eb5798442ba4ac0ac3b31ff3d6
aneridesai23 <45923314+aneridesai23@users.noreply.github.com>
2020-04-19 16:39:22 -0400

    Update server.php

--- Code/server.php
+++ Code/server.php
@@ -69,6 +69,7 @@ if (isset($_POST['signup_user'])) {
   // Finally, register user if there are no errors in the form
   if (count($errors) == 0) {
   	$password = md5($password_1);//encrypt the password before saving in the database
+	$answer = md5($answer); //added the security question encryption
 	
 	if($role === "Driver"){
     		  	$query ="INSERT INTO requests (username, email, password, role, secAns, companyN) 
